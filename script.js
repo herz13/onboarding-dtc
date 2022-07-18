c10012906b1f6abbb4dbd340f111dce673a1325d
Mariana <msacra90@gmail.com>
2022-07-18 18:08:55 +0100

    Adding endpoint and search params

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
+const url = 'http://localhost:3000/universities';
 const xhttp = new XMLHttpRequest();
-const url = 'http://localhost:3000';
 
 var button = document.getElementById('searchbutton');
 
 button.addEventListener('click', function() {
     var search = document.getElementById('searchbar').value;
-    xhttp.open("GET", search, true);
-    xhttp.send();
+    xhttp.open("GET", url);
+    xhttp.send(search);
 });
